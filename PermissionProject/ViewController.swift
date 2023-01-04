c45f292abfe510d982862a649ddc59c82c51fc65
Muratcancftc <107050541+Muratcancftc@users.noreply.github.com>
2023-01-04 17:00:15 +0300

    Initial Commit

---
+++ PermissionProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PermissionProject
+//
+//  Created by Muratcan Çiftçi on 4.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
